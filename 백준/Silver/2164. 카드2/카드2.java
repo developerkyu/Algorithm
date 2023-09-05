169bc21a592c624e43c322ec7ca572f30e17a05b
developerkyu <104983785+developerkyu@users.noreply.github.com>
2023-09-05 13:10:11 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 53392 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+//		queue의 사이즈 N 입력
+//		queue 생성
+//		while (queue의 길이가 2보다 크면) {
+//			queue.remove()
+//			int n = queue.poll()
+//			queue.add(n)
+//		}
+//		
+//		while (queue의 길이가 2이면) {
+//			queue.remove()
+//		}
+//		
+//		System.out.println(queue.peek())
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for (int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+		
+		while (queue.size() > 2) {
+			queue.remove();
+			int n = queue.poll();
+			queue.add(n);
+		}
+		
+		while (queue.size() == 2) {
+			queue.remove();
+		}
+		
+		System.out.println(queue.peek());
+	}
+}
