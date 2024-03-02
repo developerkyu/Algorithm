b6d7eedcd6d352b0015a73311c04cb225db34d03
이규진 <104983785+jinious94@users.noreply.github.com>
2024-03-02 23:28:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1000 ms, Memory: 88736 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,66 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int cnt;
-	static int[] checked;
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		
-		checked = new int [N + 1];
-		
-		for (int i = 0; i < N + 1; i++) {
-			graph.add(new ArrayList<Integer>());
+class Main {
+    static ArrayList<Integer> [] A;
+    static int [] visited;
+    static int count = 1;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int node = Integer.parseInt(st.nextToken());
+        int edge = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        
+        A = new ArrayList [node + 1];
+        visited = new int [node + 1];
+        
+        for (int i = 1; i < node + 1; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
+        
+        for (int i = 0; i < edge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            A[u].add(v);
+            A[v].add(u);
+        }
+        
+        for (int i = 1; i < node + 1; i++) {
+			Collections.sort(A[i], Collections.reverseOrder());
 		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			
-			graph.get(u).add(v);
-			graph.get(v).add(u);
-		}
-		
-		for (int i = 1; i < N + 1; i++) {
-			Collections.sort(graph.get(i), Collections.reverseOrder());
-		}
-		
-		cnt = 1;
-		DFS(S);
-		
-		StringBuffer sb = new StringBuffer();
-		
-		for (int i = 1; i < checked.length; i++) {
-			sb.append(checked[i]).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
-
-	private static void DFS(int node) {
-		checked[node] = cnt;
-		
-		for (int i = 0; i < graph.get(node).size(); i++) {
-			int newNode = graph.get(node).get(i);
-			
-			if (checked[newNode] == 0) {
-				cnt++;
-				DFS(newNode);
-			}
-		}
-		
-	}
-
+        
+        DFS(start);
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 1; i < node + 1; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+    public static void DFS(int v) {
+        if (visited[v] != 0) return;
+        
+        visited[v] = count++;
+        
+        for (int i : A[v]) {
+            if (visited[i] == 0) {
+                DFS(i);
+            }
+        }
+    }
 }
