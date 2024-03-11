7916a876610997dad007febf1349c71f0cd216a8
이규진 <104983785+jinious94@users.noreply.github.com>
2024-03-11 15:35:35 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.07 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        
+		int length = attacks.length;
+        int finalAttack = attacks[length - 1][0];
+        int[] damages = new int [finalAttack + 1];
+        
+        int hp = health;
+        int count = 0;
+        int time = bandage[0];
+        int healPerSec = bandage[1];
+        int plusHeal = bandage[2];
+        
+        boolean survive = true;
+        
+        for (int i = 0; i < length; i++) {
+        	int index = attacks[i][0];
+            damages[index] = attacks[i][1];
+        }
+        
+        for (int i = 0; i <= finalAttack; i++) {
+            if (damages[i] != 0) {
+                hp -= damages[i];
+                count = 0;
+                if (hp <= 0) {
+                    survive = false;
+                    break;
+                }
+            } else {
+                if (hp < health) {
+                    hp += healPerSec;
+                    if (hp >= health) hp = health;
+                    count++;
+                    if (count == time) {
+                        hp += plusHeal;
+                        if (hp > health) {
+                            hp = health;
+                        }
+                        count = 0;
+                    }
+                } else if (hp >= health) {
+                	count++;
+                	continue;
+                };
+            }
+        }
+        
+        if (!survive) return -1;
+        else return hp;
+    }
+}
